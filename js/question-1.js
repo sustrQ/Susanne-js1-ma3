4c53d3c2d67dd2e25e55289c9d73492ed4bae23e
sustrQ <sustrQ@users.noreply.github.com>
2021-02-07 09:00:21 +0100

    .

---
+++ js/question-1.js
@@ -0,0 +1,9 @@
+// Question 1
+
+const division = (a, b) => {
+  return a % b;
+};
+
+const result = division(5, 3);
+
+console.log(result);
--- js/question-2.js
+++ js/question-2.js
@@ -1,13 +1,3 @@
-// Question 1
-
-const division = (a, b) => {
-  return a % b;
-};
-
-const result = division(5, 3);
-
-console.log(result);
-
 // Question 2
 
 const url =
